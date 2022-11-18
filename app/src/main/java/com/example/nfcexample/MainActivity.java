9c76120734701094797a624785a95488221353f0
Carlos Miguel Alonso <1998cma@gmail.com>
2022-11-18 16:45:29 +0100

    Basic NFC read

--- app/src/main/java/com/example/nfcexample/MainActivity.java
+++ app/src/main/java/com/example/nfcexample/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.nfcexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "Device does not support NFC", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!nfcAdapter.isEnabled()) {
+            Toast.makeText(this, "NFC disabled", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Toast.makeText(this, tag.toString(), Toast.LENGTH_SHORT).show();
+        }
+        super.onNewIntent(intent);
     }
 }
